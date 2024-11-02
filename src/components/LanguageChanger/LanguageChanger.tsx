883cd02e59f8a32a49ba927b469e1c17e5782253
aeiyanzetsol <164330119+aeiyanZetsol@users.noreply.github.com>
2024-11-02 21:43:44 +0500

    imp(Header) UI and code Improvement  #2

---
+++ src/components/LanguageChanger/LanguageChanger.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { MdKeyboardArrowDown, MdKeyboardArrowUp } from "react-icons/md";
+
+// Define props type
+interface LanguageChangerProps {
+  className?: string;
+  color?: string;
+}
+
+const LanguageChanger: React.FC<LanguageChangerProps> = ({
+  className,
+  color = "background",
+}) => {
+  const [langaugeDropDown, setLangaugeDropDown] = useState(false);
+
+  const handleLanguageChange = (newLocale: string) => {
+    // Set a cookie and update the URL here (uncomment if needed)
+    // document.cookie = `locale=${newLocale}; path=/`;
+    // router.replace(`/${newLocale}${pathname}`);
+    setLangaugeDropDown(!langaugeDropDown);
+  };
+
+  return (
+    <div className={`${className}`}>
+      <div
+        onClick={() => setLangaugeDropDown(!langaugeDropDown)}
+        className={`cursor-pointer text-${color} text-xl flex items-center`}
+      >
+        EN{" "}
+        {!langaugeDropDown ? (
+          <MdKeyboardArrowDown className={`text-${color}`} />
+        ) : (
+          <MdKeyboardArrowUp className={`text-${color}`} />
+        )}
+      </div>
+
+      {langaugeDropDown && (
+        <div className="absolute top-full left-0 mt-2 border border-black bg-background py-2 px-7 space-y-4 shadow-lg">
+          <p
+            className="hover:text-primary cursor-pointer"
+            onClick={() => handleLanguageChange("en")}
+          >
+            EN
+          </p>
+          <p
+            className="hover:text-primary cursor-pointer"
+            onClick={() => handleLanguageChange("id")}
+          >
+            ID
+          </p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default LanguageChanger;
---
+++ src/components/SideFot/SideFot.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+interface sidFotProps {
+  // Add any custom props you might need, like a custom class name or additional content
+  className?: string;
+}
+
+const sidFot: React.FC<sidFotProps> = ({ className }) => {
+  return (
+    <footer
+      className={`bg-secondary py-14 flex flex-col justify-center items-center ${className}`}
+    >
+      <div className="text-center text-2xl lg:text-4xl font-bold text-primary ">
+        Show Us Your @Umahshantivilla
+      </div>
+      <p className="text-center text-primary mt-4 sm:text-sm">
+        Our guests always travel the world in style. Share your own experiences
+        using the username @Kempinskibali.
+      </p>
+      <div className="flex mt-8">
+        <button className="bg-transparent font-bold text-sm text-primary border border-primary px-4 py-1 rounded-md mr-4">
+          CONTACT US
+        </button>
+        <button className="bg-primary font-bold text-sm text-white px-4 py-2 rounded-md box-border">
+          RESERVE NOW
+        </button>
+      </div>
+    </footer>
+  );
+};
+
+export default sidFot;
