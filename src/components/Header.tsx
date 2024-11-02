b49fd11af8c6b3e2cc5c678b8a4fb6221ada2e1a
hammadShamir <hammadshamir7@gmail.com>
2024-11-02 17:16:58 +0500

    imp(Header) UI and code Improvement

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import logo from "../../public/assests/web-logo.png";
 import Image from "next/image";
 import ModalSidebar from "./Sidebar/Sidebar";
 import { MdKeyboardArrowDown } from "react-icons/md";
@@ -28,15 +27,14 @@ const Header = () => {
   };
 
   return (
-    <nav
-      className={`fixed top-0 left-0 w-full ${
-        isScrolled ? "bg-white" : "bg-transparent"
-      } z-50 transition-all duration-500 ease-in-out`}
+    <header
+      className={`fixed top-0 left-0 w-full px-6 xl:px-0 ${isScrolled ? "py-2 bg-white" : "py-4 bg-transparent"
+        } z-50 transition-all duration-500 ease-in-out`}
     >
       {" "}
       {/* Added py-5 for extra padding */}
-      <div className="max-w-full mx-auto px-4 sm:px-6 lg:px-6">
-        <div className="flex justify-between items-center h-20">
+      <div className="max-w-screen-xl mx-auto">
+        <div className="w-full relative flex justify-between items-center">
           {/* Hamburger Menu Button */}
           <div className="flex space-x-10">
             <button
@@ -45,19 +43,15 @@ const Header = () => {
             >
               {/* Line 1 */}
               <div
-                className={`absolute w-full h-1 ${
-                  isScrolled ? "bg-black" : "bg-white"
-                } transition-transform duration-300 ${
-                  isOpen ? "rotate-45" : "-translate-y-2"
-                }`}
+                className={`absolute w-full h-1 ${isScrolled ? "bg-black" : "bg-white"
+                  } transition-transform duration-300 ${isOpen ? "rotate-45" : "-translate-y-2"
+                  }`}
               ></div>
               {/* Line 2 */}
               <div
-                className={`absolute w-full h-1 ${
-                  isScrolled ? "bg-black" : "bg-white"
-                } transition-transform duration-300 ${
-                  isOpen ? "-rotate-45" : "translate-y-2"
-                }`}
+                className={`absolute w-full h-1 ${isScrolled ? "bg-black" : "bg-white"
+                  } transition-transform duration-300 ${isOpen ? "-rotate-45" : "translate-y-2"
+                  }`}
               ></div>
             </button>
             <div className="text-white text-xl flex items-center">
@@ -66,12 +60,12 @@ const Header = () => {
           </div>
 
           {/* Logo */}
-          <div className="block ">
+          <div className="block">
             {" "}
             {isScrolled ? (
               <p className="text-xl font-secondary ">UMAHH SHANTI </p>
             ) : (
-              <Image src={logo} alt="Logo" className="h-20 w-auto mt-11 " />
+              <Image src={'/assets/img/logo.png'} alt="Logo" height={70} width={70} className="absolute top-0" />
             )}
             {/* Added flex-grow for centering */}
           </div>
@@ -80,16 +74,14 @@ const Header = () => {
           <div className="flex justify-center items-center gap-10">
             <a
               href="#"
-              className={` ${
-                isScrolled ? "text-black" : "text-white"
-              } hidden lg:inline`}
+              className={` ${isScrolled ? "text-black" : "text-white"
+                } hidden lg:inline`}
             >
               SIGN IN
             </a>
             <button
-              className={`border border-white text-white ${
-                isScrolled ? "bg-primary" : "text-white"
-              }  px-4 py-2 rounded-md hover:bg-primary`}
+              className={`border border-white text-white ${isScrolled ? "bg-primary" : "text-white"
+                }  px-4 py-2 rounded-md hover:bg-primary`}
             >
               <span className="hidden lg:inline">Book Now</span>
               <span className="inline lg:hidden">Book</span>
@@ -98,8 +90,8 @@ const Header = () => {
         </div>
       </div>
       {/* Modal Sidebar */}
-      <ModalSidebar isOpen={isOpen} toggleSidebar={toggleSidebar} />
-    </nav>
+      <ModalSidebar isOpen={isOpen} toggleSidebar={toggleSidebar} isScrolled={isScrolled} />
+    </header>
   );
 };
 
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,18 +1,17 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { FaTimes } from "react-icons/fa";
-import logo from "../../../public/assests/web-logo.png";
-import Image from "next/image";
 import { MdKeyboardArrowDown } from "react-icons/md";
 
 interface ModalSidebarProps {
   isOpen: boolean;
+  isScrolled: boolean;
   toggleSidebar: () => void;
 }
 
 const ModalSidebar: React.FC<ModalSidebarProps> = ({
   isOpen,
   toggleSidebar,
+  isScrolled
 }) => {
   const [showCloseButton, setShowCloseButton] = useState(false);
 
@@ -33,77 +32,72 @@ const ModalSidebar: React.FC<ModalSidebarProps> = ({
   }, [isOpen]);
 
   return (
-    <>
-      <div
-        className={`fixed inset-0 bg-white z-50 transition-opacity duration-2000 ease-in-out ${
-          isOpen ? "opacity-100" : "opacity-0 pointer-events-none"
+    <section
+      className={`fixed inset-0 bg-white z-50 transition-opacity duration-2000 ease-in-out px-6 xl:px-0 ${isOpen ? "block opacity-100" : "none opacity-0 pointer-events-none"
         }`}
-        style={{ display: isOpen ? "block" : "none" }}
-      >
-        <div
-          className={`fixed top-0 left-0 w-full
-            bg-white
+    >
+      <div
+        className={`max-w-screen-xl mx-auto
           z-50 transition-all duration-500 ease-in-out`}
-        >
-          {" "}
-          {/* Added py-5 for extra padding */}
-          <div className="max-w-full mx-auto px-4 sm:px-6 lg:px-6">
-            <div className="flex justify-between items-center h-20">
-              {/* Hamburger Menu Button */}
-              <div className="flex space-x-10">
-                <button
-                  onClick={toggleSidebar}
-                  className="relative w-10 h-8 flex flex-col justify-center items-center group"
-                >
-                  {/* Line 1 */}
-                  <div
-                    className={`absolute w-full h-1 bg-black transition-transform duration-300 ${
-                      showCloseButton ? "rotate-45" : "-translate-y-2"
+      >
+        {" "}
+        {/* Added py-5 for extra padding */}
+        <div className={`max-w-full mx-auto py-2 ${isScrolled ? 'py-2' : 'py-4'}`}>
+          <div className="relative flex justify-between items-center">
+            {/* Hamburger Menu Button */}
+            <div className="flex space-x-10">
+              <button
+                onClick={toggleSidebar}
+                className="relative w-10 h-8 flex flex-col justify-center items-center group"
+              >
+                {/* Line 1 */}
+                <div
+                  className={`absolute w-full h-1 bg-black transition-transform duration-300 ${showCloseButton ? "rotate-45" : "-translate-y-2"
                     }`}
-                  ></div>
-                  {/* Line 2 */}
-                  <div
-                    className={`absolute w-full h-1 
+                ></div>
+                {/* Line 2 */}
+                <div
+                  className={`absolute w-full h-1 
                       bg-black
-                    transition-transform duration-300 ${
-                      showCloseButton ? "-rotate-45" : "translate-y-2"
+                    transition-transform duration-300 ${showCloseButton ? "-rotate-45" : "translate-y-2"
                     }`}
-                  ></div>
-                </button>
-                <div className="text-white text-xl flex items-center">
-                  EN <MdKeyboardArrowDown className="text-white" />
-                </div>
-              </div>
-
-              {/* Logo */}
-              <div className="block ">
-                {" "}
-                <Image src={logo} alt="Logo" className="h-20 w-auto mt-11 " />
-                {/* Added flex-grow for centering */}
-              </div>
-
-              {/* Desktop Links */}
-              <div className="flex justify-center items-center gap-10">
-                <a
-                  href="#"
-                  className={` "text-black" 
-                   hidden lg:inline`}
-                >
-                  SIGN IN
-                </a>
-                <button
-                  className={`border border-white text-white bg-primary" 
-                  }  px-4 py-2 rounded-md hover:bg-primary`}
-                >
-                  <span className="hidden lg:inline">Book Now</span>
-                  <span className="inline lg:hidden">Book</span>
-                </button>
+                ></div>
+              </button>
+              <div className="text-white text-xl flex items-center">
+                EN <MdKeyboardArrowDown className="text-white" />
               </div>
             </div>
+
+            {/* Logo */}
+            <div className="block ">
+              {" "}
+              <p className="text-xl font-secondary ">UMAHH SHANTI </p>
+              {/* Added flex-grow for centering */}
+            </div>
+
+            {/* Desktop Links */}
+            <div className="flex justify-center items-center gap-10">
+              <a
+                href="#"
+                className={` "text-black" 
+                   hidden lg:inline`}
+              >
+                SIGN IN
+              </a>
+              <button
+                className={`border border-white text-white bg-primary" 
+                  }  px-4 py-2 rounded-md hover:bg-primary`}
+              >
+                <span className="hidden lg:inline">Book Now</span>
+                <span className="inline lg:hidden">Book</span>
+              </button>
+            </div>
           </div>
-          {/* Modal Sidebar */}
         </div>
-        <ul className="mt-24 space-y-8 px-8">
+        {/* Modal Sidebar */}
+      </div>
+      <div className="max-w-screen-xl mx-auto mt-24">
+        <ul className="space-y-8">
           <li>
             <a href="#" className="text-2xl text-black hover:text-gray-500">
               Overview
@@ -131,7 +125,7 @@ const ModalSidebar: React.FC<ModalSidebarProps> = ({
           </li>
         </ul>
       </div>
-    </>
+    </section>
   );
 };
 
