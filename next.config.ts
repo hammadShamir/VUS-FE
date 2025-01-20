5deb637a8db3e84f178fa0765428800a8c431f9e
hammadShamir <hammadshamir7@gmail.com>
2025-01-20 12:00:47 +0500

    fix(#2)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig: import("next").NextConfig = {
   images: {
-    domains: ["lh3.googleusercontent.com", "storage.googleapis.com","cdninstagram.com"],
+    domains: ["lh3.googleusercontent.com", "storage.googleapis.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "scontent-iad3-*.cdninstagram.com",
+      },
+    ],
   },
 };
 
