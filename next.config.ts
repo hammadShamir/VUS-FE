8552d1dc1736dffffa1f8778f311189b75e310ee
hammadShamir <hammadshamir7@gmail.com>
2025-01-20 11:49:21 +0500

    fix(Domain)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig: import("next").NextConfig = {
   images: {
-    domains: ["lh3.googleusercontent.com", "storage.googleapis.com","scontent-iad3-1.cdninstagram.com"],
+    domains: ["lh3.googleusercontent.com", "storage.googleapis.com","*.cdninstagram.com"],
   },
 };
 
