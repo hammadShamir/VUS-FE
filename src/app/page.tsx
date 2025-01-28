ac83ba1fc419e42f4b6be0cdc6b625abe0ee14a5
hammadShamir <hammadshamir7@gmail.com>
2025-01-28 11:07:27 +0500

    feat(EmbeddedPost): Instagram section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Footer from "@/components/common/Footer";
 import Header from "@/components/common/Header/Header";
 import RoomsExplore from "@/components/RoomExplore";
 import PromoBanner from "@/components/PromoBanner";
-// import EmbeddedPost from "@/components/EmbeddedPost";
+import EmbeddedPost from "@/components/EmbeddedPost";
 
 export default function Home() {
   return (
@@ -22,7 +22,7 @@ export default function Home() {
       <PromoBanner />
       <RoomsExplore />
       <Testimonials />
-      {/* <EmbeddedPost /> */}
+      <EmbeddedPost />
       <CallForAction />
       <Footer />
     </>
--- src/components/EmbeddedPost.tsx
+++ src/components/EmbeddedPost.tsx
@@ -2,36 +2,33 @@ import React from "react";
 // import { Button } from "./ui/button";
 import InstagramSlider from "./InstagramSlider";
 import Link from "next/link";
+import Container from "./common/Container";
 
 const EmbeddedPost = () => {
   return (
-    <section className=" bg-secondary py-8 px-6 xl:px-0">
-      <div className="max-w-screen-lg mx-auto flex flex-col md:flex-row md:justify-between gap-x-12 gap-y-6">
-        <div className="space-y-4 lg:w-2/4">
-          <h2 className="font-[family-name:var(--font-primary)] text-2xl font-bold tracking-tight text-foreground md:text-4xl">
-            Follow us on instagram
-          </h2>
-          <p className="text-foreground font-[family-name:var(--font-secondary)] text-base">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-            porttitor tellus vel mauris scelerisque accumsan. Maecenas quis nunc
-            sed sapien dignissim pulvinar. Se d at gravida ligula, eget
-            hendrerit nisi. Pellentesque at congue mauris. posuere finibus
-            risus.{" "}
-          </p>
-          {/* <Button variant={"outline"}>Instagram</Button> */}
-          <div>
-            <Link
-              href={"/facilities"}
-              className="text-base rounded-md bg-transparent border border-primary text-primary px-8 py-2 hover:bg-primary hover:text-background"
-            >
-              INSTAGRAM
-            </Link>
+    <section className=" bg-secondary">
+      <Container style="flex flex-col lg:flex-row md:justify-between gap-x-12 gap-y-6 py-4">
+          <div className="space-y-4 w-full lg:w-[30%] flex flex-col justify-center">
+            <h2 className="font-[family-name:var(--font-primary)] text-2xl font-bold tracking-tight text-foreground md:text-4xl">
+              Follow us on instagram
+            </h2>
+            <p className="text-foreground font-[family-name:var(--font-secondary)] text-base">
+            Discover the moments that inspire us daily! From stunning visuals to behind-the-scenes highlights, our Instagram is your front-row seat to everything happening. Follow us for exclusive updates, vibrant stories, and a closer connection to our journey. Join the community today and never miss out on the excitement!
+            </p>
+            {/* <Button variant={"outline"}>Instagram</Button> */}
+            <div>
+              <Link
+                href={"/facilities"}
+                className="text-base rounded-md bg-transparent border border-primary text-primary px-8 py-2 hover:bg-primary hover:text-background"
+              >
+                INSTAGRAM
+              </Link>
+            </div>
           </div>
-        </div>
-        <div className="h-full lg:w-2/4">
-          <InstagramSlider />
-        </div>
-      </div>
+          <div className="h-full w-full lg:w-[70%]">
+            <InstagramSlider />
+          </div>
+      </Container>
     </section>
   );
 };
